a9206ae3627cd2e978660a950c06ad69d47fc81e
Rishi Khan <rishfilet@pop-os.localdomain>
2020-06-04 18:45:23 -0400

    Changed version of chromedriver to 2.37 and chromium binary to 1.0.0.35

--- test_selenium.py
+++ test_selenium.py
@@ -1,4 +1,7 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
 import os
 
 def handler(event, context):
@@ -6,6 +9,7 @@ def handler(event, context):
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--disable-gpu')
+    chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--window-size=1280x1696')
     chrome_options.add_argument('--user-data-dir=/tmp/user-data')
     chrome_options.add_argument('--hide-scrollbars')
@@ -18,9 +22,17 @@ def handler(event, context):
     chrome_options.add_argument('--homedir=/tmp')
     chrome_options.add_argument('--disk-cache-dir=/tmp/cache-dir')
     chrome_options.add_argument('user-agent=Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36')
-    chrome_options.binary_location = os.getcwd() + "/bin/headless-chromium"
+    chrome_options.binary_location = os.getcwd() + "/headless-chromium"
 
-    driver = webdriver.Chrome(chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.getcwd()+"/chromedriver",chrome_options=chrome_options)
+
+    driver.get("https://www.brainyquote.com/")
+    wait = WebDriverWait(driver, 10)
+    search = driver.find_element_by_id("hmSearch")
+    search.send_keys("Bruce Lee" + Keys.RETURN)
+    results = driver.find_elements_by_class_name("m-brick")
+    print(results[0])
+
 
 def test(event, context):
     print("Hello_world")
